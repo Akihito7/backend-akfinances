09438d4b56967ffd05c5c27df62e283dd4cf2e33
akihito7 <akihitopro7@gmail.com>
2023-12-20 23:42:07 -0300

    adicionado migrations para criar tabelas

---
+++ src/database/migrations/20231221023205_create-table-transactions.js
@@ -0,0 +1,11 @@
+exports.up = knex => knex.schema.createTable("transactions", table => {
+    table.increments("id"),
+    table.text("name").notNullable();
+    table.text("category").notNullable();
+    table.text("type").notNullable();
+    table.text("date").notNullable();
+    table.integer("user_id").unsigned();
+    table.foreign("user_id").references("users.id").onDelete("CASCADE")
+});
+
+exports.down = knex => knex.schema.dropTable("transactions");
